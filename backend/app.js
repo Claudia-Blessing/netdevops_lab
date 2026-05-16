cd921f274e3f9bf1ee76a337e0d435d83a227ed9
Claudia-Blessing <claudiablessing.adjafo@gmail.com>
2026-05-16 20:17:32 +0200

    Prometheus + grafana

--- backend/app.js
+++ backend/app.js
@@ -14,3 +14,14 @@ app.listen(3000, () => {
 
     console.log('Server running on port 3000');
 });
+
+
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+});
